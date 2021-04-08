4a1d533e664ee4bd29db064d434c4f80f7186f60
Ashya Singh <ashyasingh@Ashyas-MacBookPro.local>
2021-04-07 21:07:57 -0400

    button add

--- prody-master/prody_fe/src/Console/Surveys/NewSurveyPage.js
+++ prody-master/prody_fe/src/Console/Surveys/NewSurveyPage.js
@@ -1,4 +1,4 @@
-import React, { useContext , useEffect} from 'react';
+import React, { useContext , useEffect, useState} from 'react';
 import {useHistory, useParams} from 'react-router-dom';
 import {ConsoleContext} from "../ConsoleContext";
 //got these from Ingredients.js
@@ -10,7 +10,6 @@ import {CONSTANTS} from '../../utils/constants';
 //
 import SearchBar from './SearchBar';
 import './NewSurveyPage.css';
-
 
  const NewSurveyPage = () => {
 
@@ -36,10 +35,39 @@ import './NewSurveyPage.css';
            </div>
        )}
    };
+   /*
+    const Button = styled.button`
+    background-color: black;
+    color: white;
+    font-size: 20px;
+    padding: 10px 60px;
+    border-radius: 5px;
+    margin: 10px 0px;
+    cursor: pointer;
+    &:disabled {
+        color: grey;
+        opacity: 0.7;
+        cursor: default;
+    }
+    `;*/
+    function sayHello() {
+        alert('hi');
+    }
+    function submit() {
+        alert('Submitted!');
+        }
+  
    return (
            <div className='NewSurveyPagePageSite'>
                <SearchBar/>
                {showCards()}
+               <button onClick={sayHello}>
+                    Add a new question!
+               </button>
+
+               <button onClick={submit}>
+                    Submit survey!
+               </button>
            </div>
    )
 
